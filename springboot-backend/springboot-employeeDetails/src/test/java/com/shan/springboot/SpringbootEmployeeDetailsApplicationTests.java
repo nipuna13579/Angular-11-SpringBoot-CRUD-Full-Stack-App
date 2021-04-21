9ef20113fdfeb085a441ff90bc752d6be9f8607e
nipuna13579 <70423102+nipuna13579@users.noreply.github.com>
2021-04-21 22:48:58 +0530

    Add files via upload

---
+++ springboot-backend/springboot-employeeDetails/src/test/java/com/shan/springboot/SpringbootEmployeeDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shan.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmployeeDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
